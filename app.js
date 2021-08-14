025dec19f547a32bc80d516f028d97a031415648
Eric Lich <eric@Erics-Air.fibertel.com.ar>
2021-08-14 15:54:08 -0300

    mobile first approach

--- app.js
+++ app.js
@@ -1,155 +1,71 @@
-
-
-const categorias = [
-    [
-        {
-            "microprocesadores": [
-                "Socket AMD AM4",
-                "Socket Intel 1151",
-                "Socket Intel 1200"
-            ]
-        },
-        {
-            "motherboards": [
-                "Mother AMD AM4",
-                "Mother Intel 1151",
-                "Mother Intel 1200"
-            ]
-        },
-        {
-            "memorias ram": [
-                "Memorias Para Pc (UDIMM)",
-                "Memorias Para Notebook (SODIMM)"
-            ]
-        },
-        {
-            "placas de video": [
-                "Placas de Video AMD Radeon",
-                "Placas de Video Nvidia"
-            ]
-        }
-    ],
-    [
-        {
-            "almacenamiento": [
-                "Discos Externos USB",
-                "Discos Rígidos HDD para Notebook",
-                "Discos Rígidos HDD para PC",
-                "Discos Sólidos SSD Internos",
-                "Memorias SD - Pendrives"
-            ]
-        },
-        {
-            "refrigeración": [
-                "Air Cooling",
-                "Water Cooling",
-                "Pasta Térmica",
-                "Fan 120mm",
-                "Fan 140mm",
-                "Accesorios"
-            ]
-        },
-        {
-            "televisores": [
-                "Smart TV"
-            ]
-        },
-    ],
-    [
-        {
-            "fuentes": [
-                "Sin certificado",
-                "80 Plus White",
-                "80 Plus Bronce",
-                "80 Plus Gold"
-            ]
-        },
-        {
-            "gabinetes": [
-                "Full Tower",
-                "Mid Tower",
-                "Mini Tower"
-            ]
-        },
-        {
-            "sillas": [
-                "Sillas gamer"
-            ]
-        },
-        {
-            "tablets":[
-                "Tabletas Digitalizadoras"
-            ]
-        },
-    ],
-    [
-        {
-            "accesorios": [
-                "Soportes de GPU",
-                "Cabls Mallados",
-                "Tiras Led",
-                "Estabilizador y UPS",
-                "Routers",
-                "Mochilas y Bolsos"
-            ]
-        },
-        {
-            "sistema operativo":[
-                "Microsoft Windows"
-            ]
-        },
-        {
-            "capturadoras de video": [
-                "Capturaoras de Video"
-            ]
-        },
-        {
-            "smartwatch - smartband": [
-                "SmartBand"
-            ]
-        },
-        {
-            "pequeños electrodomesticos": [
-                "Termometros",
-                "TV Stick - TV Box"
-            ]
-        }
-    ]  
-]
-const lados = [
-    "left",
-    "centerLeft",
-    "centerRight",
-    "right",
-]
-
-let prueba = document.querySelector(".prueba")
-
-//prueba.className = "hidden"
-
-function mostrar(e){
-    e.preventDefault();
-    for(let i = 0; i < categorias.length; i++) {
-        console.log(categorias[i]);
-        prueba.appendChild(generateLado(categorias[i], i));  
-    }
+const buttonCarrito = document.querySelector(".carrito");
+const carritoMobile = document.querySelector(".carrito-drop-mobile");
+//animacion de carrito mobile
+buttonCarrito.addEventListener('click', () => {
+    let claseActive = "carrito-drop-active"
     
-    const generateLado = (listado, num) => {return generateZone(listado[num], num)}
-    
-    const generateZone = (zone, pos) => {
-        let ul = `<ul class="${lados[pos]}"></ul>`;
-        ul.appendChild(`<p>${Object.keys(categorias)[pos]}</p>`)
-        for(cat of zone){
-            ul.appendChild(
-                console.log(cat)
-                `<li>${cat}</li>`
-            )
-        }
-        return ul;
+    if(!carritoMobile.classList.contains(claseActive)){
+        carritoMobile.classList.add(claseActive)
+    }else{
+        carritoMobile.classList.remove(claseActive)
     }
-}
+})
+//animacion de search mobile
+const buttonSearch = document.querySelector(".search-mobile-button");
+buttonSearch.addEventListener('click', () =>{
+    let searchMobile = document.querySelector(".search-mobile");
+    let claseActive = "search-mobile-active"
+    let claseActiveCarrito = "carrito-drop-active"
 
+    let mainBlurred = "main-blurred"
+    let main = document.querySelector(".main")
+    
+    if(!searchMobile.classList.contains(claseActive)){
+        searchMobile.classList.add(claseActive);
+        main.classList.add(mainBlurred)
+        if(carritoMobile.classList.contains(claseActiveCarrito)){
+            carritoMobile.classList.remove(claseActiveCarrito)
+        }
+    }
+    main.addEventListener('click', () => {
+        if(searchMobile.classList.contains(claseActive)){
+            searchMobile.classList.remove(claseActive)
+        }
+        main.classList.remove(mainBlurred)
+    })
+})
 
+//mostrar cruz y esconder menu hamburguesa o al reves y mostrar u ocultar menu
+const menu = document.querySelector('.hamburger-menu');
+const cruz = document.querySelector('.fa-times')
+const spans = document.querySelectorAll('.burg')
+const claseCruzActiva = "cruz-active";
+const hamburgerInactivo = "hamburger-inactive";
+
+const menuCategorias = document.querySelector('.menu-categorias');
+const menuCatActive = "menu-categorias-active"
+
+menu.addEventListener('click', () => {
+    if(!cruz.classList.contains(claseCruzActiva)){
+        cruz.classList.add(claseCruzActiva);
+        menu.classList.add(hamburgerInactivo);
+        console.log(spans)
+        for(span of spans){
+            span.classList.add(hamburgerInactivo)
+        }
+        menuCategorias.classList.add(menuCatActive)
+    }
+})
+cruz.addEventListener('click', () => {
+    if(cruz.classList.contains(claseCruzActiva)){
+        cruz.classList.remove(claseCruzActiva);
+        menu.classList.remove(hamburgerInactivo)
+        for(span of spans){
+            span.classList.remove(hamburgerInactivo)
+        }
+        menuCategorias.classList.remove(menuCatActive)
+    }
+})
 
 
 
