7fd8c397da735a482010f6ee59aea98b5d0453b1
Eric Lich <eric@Erics-Air.fibertel.com.ar>
2021-08-16 12:30:37 -0300

    responsive design

--- app.js
+++ app.js
@@ -19,6 +19,7 @@ buttonSearch.addEventListener('click', () =>{
 
     let mainBlurred = "main-blurred"
     let main = document.querySelector(".main")
+    let flechaGuardarBusqueda = document.querySelector(".guardar-search-mobile")
     
     if(!searchMobile.classList.contains(claseActive)){
         searchMobile.classList.add(claseActive);
@@ -27,7 +28,7 @@ buttonSearch.addEventListener('click', () =>{
             carritoMobile.classList.remove(claseActiveCarrito)
         }
     }
-    main.addEventListener('click', () => {
+    flechaGuardarBusqueda.addEventListener('click', () => {
         if(searchMobile.classList.contains(claseActive)){
             searchMobile.classList.remove(claseActive)
         }
@@ -41,6 +42,9 @@ const cruz = document.querySelector('.fa-times')
 const spans = document.querySelectorAll('.burg')
 const claseCruzActiva = "cruz-active";
 const hamburgerInactivo = "hamburger-inactive";
+const prodsDropdown = document.querySelectorAll('.prods-dropdown')
+const none = "none"
+const flex = "flex";
 
 const menuCategorias = document.querySelector('.menu-categorias');
 const menuCatActive = "menu-categorias-active"
@@ -49,7 +53,6 @@ menu.addEventListener('click', () => {
     if(!cruz.classList.contains(claseCruzActiva)){
         cruz.classList.add(claseCruzActiva);
         menu.classList.add(hamburgerInactivo);
-        console.log(spans)
         for(span of spans){
             span.classList.add(hamburgerInactivo)
         }
@@ -57,15 +60,54 @@ menu.addEventListener('click', () => {
     }
 })
 cruz.addEventListener('click', () => {
+    const minuses = document.querySelectorAll('.fa-minus')
+    const pluses = document.querySelectorAll('.fa-plus')
     if(cruz.classList.contains(claseCruzActiva)){
         cruz.classList.remove(claseCruzActiva);
         menu.classList.remove(hamburgerInactivo)
         for(span of spans){
             span.classList.remove(hamburgerInactivo)
         }
+
+        for(drop of prodsDropdown){
+            if(!drop.classList.contains(none)){
+                drop.classList.add(none)
+                drop.classList.remove(flex)
+            }
+        }
+
+        for(plus of pluses){
+            if(plus.classList.contains(none)){
+                plus.classList.remove(none)
+            }
+        }
+
+        for(minus of minuses){
+            if(!minus.classList.contains(none)){
+                minus.classList.add(none)
+            }
+        }
         menuCategorias.classList.remove(menuCatActive)
     }
 })
 
 
+//MOSTRAR Y OCULTAR DROPDOWNS DE CATEGORIAS
+const showOrHide = (element) => {
+    const ulDropDown = element.parentElement.children[1];
+    const plus = element.children[1].children[0];
+    const minus = element.children[1].children[1];
 
+    if(ulDropDown.classList.contains(none)){
+        ulDropDown.classList.remove(none);
+        ulDropDown.classList.add(flex)
+        plus.classList.add(none)
+        minus.classList.remove(none)
+    }else{
+        ulDropDown.classList.remove(flex)
+        ulDropDown.classList.add(none)
+        plus.classList.remove(none)
+        minus.classList.add(none)
+    }
+
+}
